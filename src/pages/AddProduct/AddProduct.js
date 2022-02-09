d6d2535b13e9b410d1381897d70d19d156506ccc
Alamin <alamin.mj.3228@gmail.com>
2022-02-09 14:20:39 +0600

    axios added
    Signed-off-by: Alamin <alamin.mj.3228@gmail.com>

--- src/pages/AddProduct/AddProduct.js
+++ src/pages/AddProduct/AddProduct.js
@@ -1,31 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import { Form, Row, Col, Button } from "react-bootstrap";
 import { IoMdPaperPlane } from "react-icons/io";
 import "./AddProduct.css";
 
 const AddProduct = () => {
-  
+  const [productName, setProductName] = useState("");
+  const [productCode, setProductCode] = useState("");
+  const [unit, setunit] = useState("");
+  const [image, setImage] = useState("");
+  useEffect(() => {
+    console.log(image);
+  }, []);
+  const submitFrom = (e) => {
+    e.preventDefault();
+    const formData = { productName, productCode, unit };
+    // formData.append("productName", productName);
+    // formData.append("productCode", productCode);
+    // formData.append("unite", unit);
+    // formData.append("image", image, image.name);
+    axios({
+      method: "post",
+      url: "http://localhost:5000/api/productlists",
+      data: formData,
+      // headers: { "content-type": "multipart/form-data" },
+    })
+      .then(function (response) {
+        //handle success
+        console.log(response);
+      })
+      .catch(function (response) {
+        //handle error
+        console.log(response);
+      });
+  };
   return (
     <div>
       <h1 className="display-4 text-primary text-center">Add Product</h1>
       {/* form */}
-      <Form className=" w-75 mx-auto mt-5">
+      <Form className=" w-75 mx-auto mt-5" onSubmit={submitFrom}>
         <Row className="mb-3">
           <Form.Group as={Col} controlId="formGridEmail">
             <Form.Label>Product Name</Form.Label>
-            <Form.Control type="text" placeholder="Product Name" />
+            <Form.Control
+              type="text"
+              placeholder="Product Name"
+              onChange={(e) => setProductName(e.target.value)}
+            />
           </Form.Group>
         </Row>
 
         <Row className="mb-3">
           <Form.Group as={Col} controlId="formGridPassword">
             <Form.Label>Product code</Form.Label>
-            <Form.Control type="number" placeholder="123" />
+            <Form.Control
+              type="number"
+              placeholder="123"
+              onChange={(e) => setProductCode(e.target.value)}
+            />
           </Form.Group>
 
           <Form.Group as={Col} controlId="formGridState">
             <Form.Label>Unit</Form.Label>
-            <Form.Select defaultValue="Choose...">
+            <Form.Select
+              defaultValue="Choose..."
+              onChange={(e) => setunit(e.target.value)}
+            >
               <option>Kg</option>
               <option>Pices</option>
               <option>Litter</option>
@@ -34,7 +75,12 @@ const AddProduct = () => {
 
           <Form.Group controlId="formFile" className="mb-3">
             <Form.Label>Product Image</Form.Label>
-            <Form.Control type="file" />
+            <Form.Control
+              type="file"
+              onChange={(e) => {
+                setImage(e.target.files[0]);
+              }}
+            />
           </Form.Group>
         </Row>
 
--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,8 +1,20 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Button, Card } from "react-bootstrap";
 import { AiOutlinePlus } from "react-icons/ai";
 import "./ProductList.css";
 const ProductList = () => {
+  // const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="container px-4">
       <h1 className="display-4 text-primary text-center">product List</h1>
@@ -43,176 +55,6 @@ const ProductList = () => {
               Edit
             </Button>
             <Button variant="danger" className="ms-2" size="sm">
-              Delete
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            className="product-list-img"
-            src="https://smartbdltd.com/wp-content/uploads/2020/05/1-1.jpg"
-          />
-          <Card.Body>
-            <Card.Title>Mosor Dal</Card.Title>
-            <Card.Text></Card.Text>
-            <Button variant="primary" size="sm">
-              Edit
-            </Button>
-            <Button variant="danger" size="sm" className="ms-2">
-              Delete
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            className="product-list-img"
-            src="https://thefinancialexpress.com.bd/uploads/1630856711.jpeg"
-          />
-          <Card.Body>
-            <Card.Title>Rice Minicate</Card.Title>
-            <Card.Text></Card.Text>
-            <Button variant="primary" size="sm">
-              Edit
-            </Button>
-            <Button variant="danger" size="sm" className="ms-2">
-              Delete
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            className="product-list-img"
-            src="https://thefinancialexpress.com.bd/uploads/1630856711.jpeg"
-          />
-          <Card.Body>
-            <Card.Title>Rice Minicate</Card.Title>
-            <Card.Text></Card.Text>
-            <Button variant="primary" size="sm">
-              Edit
-            </Button>
-            <Button variant="danger" size="sm" className="ms-2">
-              Delete
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            className="product-list-img"
-            src="https://thefinancialexpress.com.bd/uploads/1630856711.jpeg"
-          />
-          <Card.Body>
-            <Card.Title>Rice Minicate</Card.Title>
-            <Card.Text></Card.Text>
-            <Button variant="primary" size="sm">
-              Edit
-            </Button>
-            <Button variant="danger" size="sm" className="ms-2">
-              Delete
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            className="product-list-img"
-            src="https://thefinancialexpress.com.bd/uploads/1630856711.jpeg"
-          />
-          <Card.Body>
-            <Card.Title>Rice Minicate</Card.Title>
-            <Card.Text></Card.Text>
-            <Button variant="primary" size="sm">
-              Edit
-            </Button>
-            <Button variant="danger" size="sm" className="ms-2">
-              Delete
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            className="product-list-img"
-            src="https://thefinancialexpress.com.bd/uploads/1630856711.jpeg"
-          />
-          <Card.Body>
-            <Card.Title>Rice Minicate</Card.Title>
-            <Card.Text></Card.Text>
-            <Button variant="primary" size="sm">
-              Edit
-            </Button>
-            <Button variant="danger" size="sm" className="ms-2">
-              Delete
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            className="product-list-img"
-            src="https://thefinancialexpress.com.bd/uploads/1630856711.jpeg"
-          />
-          <Card.Body>
-            <Card.Title>Rice Minicate</Card.Title>
-            <Card.Text></Card.Text>
-            <Button variant="primary" size="sm">
-              Edit
-            </Button>
-            <Button variant="danger" size="sm" className="ms-2">
-              Delete
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            className="product-list-img"
-            src="https://thefinancialexpress.com.bd/uploads/1630856711.jpeg"
-          />
-          <Card.Body>
-            <Card.Title>Rice Minicate</Card.Title>
-            <Card.Text></Card.Text>
-            <Button variant="primary" size="sm">
-              Edit
-            </Button>
-            <Button variant="danger" size="sm" className="ms-2">
-              Delete
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            className="product-list-img"
-            src="https://thefinancialexpress.com.bd/uploads/1630856711.jpeg"
-          />
-          <Card.Body>
-            <Card.Title>Rice Minicate</Card.Title>
-            <Card.Text></Card.Text>
-            <Button variant="primary" size="sm">
-              Edit
-            </Button>
-            <Button variant="danger" size="sm" className="ms-2">
-              Delete
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            className="product-list-img"
-            src="https://thefinancialexpress.com.bd/uploads/1630856711.jpeg"
-          />
-          <Card.Body>
-            <Card.Title>Rice Minicate</Card.Title>
-            <Card.Text></Card.Text>
-            <Button variant="primary" size="sm">
-              Edit
-            </Button>
-            <Button variant="danger" size="sm" className="ms-2">
               Delete
             </Button>
           </Card.Body>
